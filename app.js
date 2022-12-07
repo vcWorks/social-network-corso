e3ebca360fefd024693958e471c6ebf189e188b7
VicDeveloper <vc.developworks@gmail.com>
2022-12-07 11:44:19 +0100

    impostate le rotte

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
+const router = require("./router");
 
 app.use(express.static('public'));
 app.set('views', 'views'); // il primo parametro è obbligatorio views perché parola proprietaria di express; il secondo paremetro è il nome della cartella che abbiamo creato
 app.set('view engine', 'ejs');
 
 
-app.get('/', function(req, res) {
-    res.render('home-guest');
-});
+app.use('/', router);
 
 app.listen(3000);
