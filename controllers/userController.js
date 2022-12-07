803bafa18de7d63b90795d9f95f9f9ab4b7657c6
VicDeveloper <vc.developworks@gmail.com>
2022-12-07 12:03:48 +0100

    impostato il controller

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.login = function() {
+    
+}
+
+
+exports.logout = function() {
+
+}
+
+
+exports.register = function(req, res) {
+    res.send("n'altra volta");
+}
+
+exports.home = function(req, res) {
+    res.render('home-guest');
+}
+
+
