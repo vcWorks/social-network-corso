d53400babe9d997fb706ca8c1036c1b94847460b
VicDeveloper <vc.developworks@gmail.com>
2022-12-08 11:28:22 +0100

    prima parte di implementazione della sessione e dei cookies

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
+
+// configurazione della sessione
+let sessionOptions = session({
+    secret: "Brad è molto polite",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // vale 1gg
+        httpOnly: true
+    }
+});
+//express adesso utilizza la sessione
+app.use(sessionOptions);
+
 const router = require("./router");
 
 app.use(express.urlencoded({extended: false}));
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,9 @@ const User = require('../models/User');
 exports.login = function(req, res) {
     let user = new User(req.body);
     user.login().then(function(result) {
+        req.session.user = {
+            username: user.data.usermae
+        }
         res.send(result);
     }).catch(function(err) {
         res.send(err);
@@ -26,7 +29,11 @@ exports.register = function(req, res) {
 }
 
 exports.home = function(req, res) {
-    res.render('home-guest');
+    if(req.session.user) {
+        res.send("Benvenuto sul portale!");
+    } else {
+        res.render('home-guest');
+    } 
 }
 
 
